5007031004eb0ad62ca0255a6f2f05ff88a2cd58
amadokill <caikvictor53@gmail.com>
2026-04-21 13:16:06 -0300

    configuração inicial do projeto

---
+++ Cores.py
@@ -0,0 +1,19 @@
+# Módulo de cores ANSI
+
+cores = {
+    "limpa": "\033[m",
+    "vermelho": "\033[31m",
+    "verde": "\033[32m",
+    "amarelo": "\033[33m",
+    "azul": "\033[34m",
+    "roxo": "\033[35m",
+    "ciano": "\033[36m",
+    "branco": "\033[37m",
+    "fundo_vermelho": "\033[41m",
+    "fundo_verde": "\033[42m",
+    "fundo_amarelo": "\033[43m",
+    "fundo_azul": "\033[44m",
+    "fundo_roxo": "\033[45m",
+    "fundo_ciano": "\033[46m",
+    "fundo_branco": "\033[47m",
+}
--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+import Cores
 
+
+def Msg(msg):
+    c = Cores.cores
+    r = len(msg)
+    print(Cores.cores["azul"] + "-" * r)
+    print(Cores.cores["verde"] + msg)
+    print(Cores.cores["azul"] + "-" * r)
+
+    return (
+        f"{c['amarelo']}1{c['limpa']} - {c['roxo']}Ver Pessoas Cadastradas{c['limpa']}\n"
+        f"{c['amarelo']}2{c['limpa']} - {c['roxo']}Cadastrar nova pessoa{c['limpa']}\n"
+        f"{c['amarelo']}3{c['limpa']} - {c['roxo']}Sair{c['limpa']}"
+    )
+
+
+def Resposta():
+
+    while True:
+        op = input(Cores.cores["verde"] + "Escolha uma opção: " + Cores.cores["limpa"])
+        if op == "1":
+            print("Mostrando pessoas cadastradas...")
+            break
+        elif op == "2":
+            print("Cadastrar nova pessoa...")
+            break
+        elif op == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida!")
+
+
+print(Msg("Cavalo".center(30)))
+Resposta()
